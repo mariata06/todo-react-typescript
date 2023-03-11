97e431121da62f8cd8a61a76b038d36f7da8bd37
mariata06 <slinkywizard@mail.ru>
2023-03-11 18:04:21 +0300

    Add NewTodo component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-const TodoList: React.FC = () => {
-    const todos = [{id: 't1', text: 'Finish the course'}];
-    return <ul>
-        {todos.map(todo => (
-            <li key={todo.id}>{todo.text}</li>
-        ))}
-    </ul>;
+interface TodoListProps {
+    items: {
+        id: string,
+        text: string
+    }[];
+};
+
+const TodoList: React.FC<TodoListProps> = (props) => {
+
+    return (
+        <ul>
+            {props.items.map(todo => (
+                <li key={todo.id}>{todo.text}</li>
+            ))}
+        </ul>
+    );
 };
 
 export default TodoList;
